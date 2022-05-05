a837ef629ec93d9708ed323ab11cdccefb556add
jcadavid52 <jccadavid17@misena.edu.co>
2022-05-04 22:00:37 -0500

    version 1

---
+++ src/app/pages/action/action.page.ts
@@ -0,0 +1,64 @@
+import { Component, OnInit } from '@angular/core';
+import { ActionSheetController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-action',
+  templateUrl: './action.page.html',
+  styleUrls: ['./action.page.scss'],
+})
+export class ActionPage implements OnInit {
+
+  constructor(public actionSheetCtrl: ActionSheetController) { }
+
+  ngOnInit() {
+  }
+  async presentActionSheet() {
+    const actionSheet = await this.actionSheetCtrl.create({
+      header: 'Albums',
+      backdropDismiss: false,
+      buttons: [{
+        text: 'Delete',
+        icon: 'trash-outline',
+        id: 'delete-button',
+        data: {
+          type: 'delete'
+        },
+        cssClass: 'rojo',
+        handler: () => {
+          console.log('Delete clicked');
+        }
+      }, {
+        text: 'Share',
+        icon: 'share-outline',
+        data: 10,
+        handler: () => {
+          console.log('Share clicked');
+        }
+      }, {
+        text: 'Play (open modal)',
+        icon: 'caret-forward-circle-outline',
+        data: 'Data value',
+        handler: () => {
+          console.log('Play clicked');
+        }
+      }, {
+        text: 'Favorite',
+        icon: 'heart-outline',
+        handler: () => {
+          console.log('Favorite clicked');
+        }
+      }, {
+        text: 'Cancel',
+        icon: 'close-outline',
+        role: 'cancel',
+        handler: () => {
+          console.log('Cancel clicked');
+        }
+      }]
+    });
+    await actionSheet.present();
+
+
+  }
+
+}
---
+++ src/app/pages/inicio/inicio.page.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+interface Componente{
+  icon: string;
+  name: string;
+  redirectTo: string;
+}
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.page.html',
+  styleUrls: ['./inicio.page.scss'],
+})
+export class InicioPage implements OnInit {
+
+  componentes: Componente[] = [
+    {
+      icon: 'build-outline',
+      name: 'action',
+      redirectTo: '/action'
+    },
+     {
+      icon: 'cafe-outline',
+      name: 'alert',
+      redirectTo: '/alert'
+    }
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
